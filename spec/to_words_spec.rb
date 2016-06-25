fbce55e49605926758af49e3479fdc4f82c45a42
andela-ydaniju <yusuf.daniju@andela.com>
2016-06-25 16:57:26 +0100

    add test for a stringy number

--- spec/to_words_spec.rb
+++ spec/to_words_spec.rb
@@ -1,6 +1,16 @@
 require_relative 'spec_helper'
-RSpec.describe 'Subtraction' do
-  it 'works' do
-    expect(1.to_words).to eq('one')
+RSpec.describe 'to_word' do
+  context '#to_word' do
+    it 'converts 1 to `one`' do
+      expect(1.to_words).to eq('one')
+    end
+
+    it "converts '1' to `one`" do
+      expect('1'.to_words).to eq('one')
+    end
+
+    it 'it extends Fixnum methods' do
+      expect(1.methods).to include :to_words
+    end
   end
 end
